da9103f19780b69fcdd9eafc8ffdb1be49cb63dc
Ryan White <6309098+rythwh@users.noreply.github.com>
2021-11-14 09:57:49 -0500

    Added some debug messages to SelectableManager to be used for testing when implemented.

--- Assets/Scripts/Interfaces/SelectableManager.cs
+++ Assets/Scripts/Interfaces/SelectableManager.cs
@@ -14,16 +14,20 @@ public class SelectableManager : BaseManager {
 	public static void AddSelectable(ISelectable selectable) {
 		selected.Add(selectable);
 		selectable.Select();
+
+		Debug.Log("Selected " + selectable.ToString());
 	}
 
 	public static void RemoveSelectable(ISelectable selectable) {
 		selected.Remove(selectable);
 		selectable.Deselect();
+
+		Debug.Log("Deselected " + selectable.ToString());
 	}
 
 	public override void Update() {
 		if (Input.GetMouseButtonUp(0)) {
-
+			
 		}
 
 		if (Input.GetMouseButtonUp(1)) {
