f14e00bbb6254c46565c04eabf8708aa79200a0d
Ryan White <6309098+rythwh@users.noreply.github.com>
2025-06-12 20:14:03 +0200

    Separate IUITabElement into its own file.

---
+++ Assets/Code/UI/Simulation/MVPE/UIHumanInfoPanel/Elements/Tabs/IUITabElement.cs
@@ -0,0 +1,11 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Snowship.NUI.UITab
+{
+	public interface IUITabElement
+	{
+		void SetActive(bool active);
+		UniTask Open(Transform parent);
+	}
+}
--- Assets/Code/UI/Simulation/MVPE/UIHumanInfoPanel/Elements/Tabs/UITabElement.cs
+++ Assets/Code/UI/Simulation/MVPE/UIHumanInfoPanel/Elements/Tabs/UITabElement.cs
@@ -1,18 +1,9 @@
-﻿using Cysharp.Threading.Tasks;
-using UnityEngine;
-
-namespace Snowship.NUI.UITab
+﻿namespace Snowship.NUI.UITab
 {
 	public abstract class UITabElement<T> : UIElement<T>, IUITabElement where T : UITabElementComponent
 	{
 		public void SetActive(bool active) {
 			Component.gameObject.SetActive(active);
 		}
-	}
-
-	public interface IUITabElement
-	{
-		void SetActive(bool active);
-		UniTask Open(Transform parent);
 	}
 }
